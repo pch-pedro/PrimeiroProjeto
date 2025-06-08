5d742c039b264f9f77c74ced02ab2464e17df182
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-08 19:45:23 -0300

    Configurando App.ts Livro

--- ApiGestaoBiblioteca/src/app.ts
+++ ApiGestaoBiblioteca/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import usuarioRoutes from './routes/usuario.routes';
+import livroRoutes from './routes/usuario.routes'
 
 const app = express();
 app.use(express.json());
 
 app.use('/library/usuarios', usuarioRoutes);
+app.use('/library/livros', livroRoutes);
 
 const PORT = 3090;
 app.listen(PORT, () => {
