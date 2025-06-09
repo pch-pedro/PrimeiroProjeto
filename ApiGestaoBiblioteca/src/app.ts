cb4bedecc0426ab27897fe04307986a1c1827fc5
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-08 22:53:20 -0300

    Ajustando Emprestimo App

--- ApiGestaoBiblioteca/src/app.ts
+++ ApiGestaoBiblioteca/src/app.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import usuarioRoutes from './routes/usuario.routes';
 import livroRoutes from './routes/usuario.routes';
 import estoqueRoutes   from './routes/estoque.routes';
+import emprestimoRoutes from './routes/emprestimo.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/library/usuarios', usuarioRoutes);
 app.use('/library/livros', livroRoutes);
-app.use('/library/estoque',    estoqueRoutes);
+app.use('/library/estoque', estoqueRoutes);
+app.use('/library/emprestimos', emprestimoRoutes);
 
 
 const PORT = 3090;
