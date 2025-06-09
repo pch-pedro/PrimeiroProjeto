6c0364260064d2c72a69a6c88b667a9f88e8b156
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-08 21:02:34 -0300

    Ajustando Estoque app

--- ApiGestaoBiblioteca/src/app.ts
+++ ApiGestaoBiblioteca/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import usuarioRoutes from './routes/usuario.routes';
-import livroRoutes from './routes/usuario.routes'
+import livroRoutes from './routes/usuario.routes';
+import estoqueRoutes   from './routes/estoque.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/library/usuarios', usuarioRoutes);
 app.use('/library/livros', livroRoutes);
+app.use('/library/estoque',    estoqueRoutes);
+
 
 const PORT = 3090;
 app.listen(PORT, () => {
