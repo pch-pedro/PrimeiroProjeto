6a2c59349c427f6dd1544be67f47ede33db6bcc5
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-15 22:27:43 -0300

    Restringindo Categoria Curso

--- ApiGestaoBiblioteca/src/model/Curso.ts
+++ ApiGestaoBiblioteca/src/model/Curso.ts
@@ -3,7 +3,27 @@ export class Curso{
     nome: string;
 
     constructor(id: number, nome: string){
-        this.id = id;
+        nome = nome.toLowerCase();
+        this.verificarCurso(nome);
+        this.id = this.definirId(nome);
         this.nome = nome;
+    }
+
+    definirId(nome: string): number{
+        if(nome === 'ads'){
+            return 1;
+        }
+        else if(nome === 'pedagogia'){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+
+    verificarCurso(nome: string): void{
+        if(nome !== 'ads' && nome !== 'pedagogia' && nome !== 'administração'){
+            throw new Error(`Curso: '${nome}' não está disponível por enquanto. Certifique que o usuário seja de um desses cursos: ads, pedagogia e administração esse último com as acentuações corretas`);
+        }
     }
 }
