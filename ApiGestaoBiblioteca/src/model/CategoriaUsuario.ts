d40fd2d0ca84aab8ba987779bba7af877de954be
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-15 22:11:33 -0300

    Restringindo Categoria Usuário

--- ApiGestaoBiblioteca/src/model/CategoriaUsuario.ts
+++ ApiGestaoBiblioteca/src/model/CategoriaUsuario.ts
@@ -3,7 +3,27 @@ export class CategoriaUsuario{
     nome: string;
 
     constructor(id: number, nome: string){
-        this.id = id;
+        nome = nome.toLowerCase();
+        this.verificarCategoria(nome);
+        this.id = this.definirId(nome);
         this.nome = nome;
+    }
+
+    definirId(nome: string): number{
+        if(nome === 'professor'){
+            return 1;
+        }
+        else if(nome === 'aluno'){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+
+    verificarCategoria(nome: string): void{
+        if(nome !== 'professor' && nome !== 'aluno' && nome !== 'bibliotecário'){
+            throw new Error(`Categoria de Usuário '${nome}' não é permitida. Certifique que a categoria do usuário seja uma dessas opções: 'professor', 'aluno' e 'bibliotecário' esse último com acento agudo no 'a'`);
+        }
     }
 };
