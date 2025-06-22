2bd0717ecad6ddc5fe823afb9ecbb2e049b84f4b
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-22 18:52:59 -0300

    Adicionando funções atualizar e Listar no Livro Repository

--- ApiGestaoBiblioteca/src/repository/LivroRepository.ts
+++ ApiGestaoBiblioteca/src/repository/LivroRepository.ts
@@ -1,35 +1,94 @@
 import { Livro } from "../model/Livro";
+import { CategoriaLivro } from "../model/CateoriaLivro";
 
 export class LivroRepository{
-    livros: Livro [] = [];
-    proximoId = 1;
+    private static instance: LivroRepository;
 
-    salvar(livro: {titulo: string, autor: string, editora: string, edicao: string, isbn: string, categoria_id: number}): Livro{
-        const novoLivro = new Livro(
-            this.proximoId,
-            livro.titulo,
-            livro.autor,
-            livro.editora,
-            livro.edicao,
-            livro.isbn,
-            livro.categoria_id
-        );
+    private livros: Livro[] = [];
 
-        this.proximoId++;
-        this.livros.push(novoLivro);
-
-        return novoLivro;
+    private constructor(){
+        this.livros = [];
     }
 
-    listar(): Livro[] {
-        return this.livros;
+    static getInstance(): LivroRepository{
+        if(!LivroRepository.instance){
+            LivroRepository.instance = new LivroRepository();
+        }
+        return LivroRepository.instance;
+    }
+
+    salvar(livro: Livro): Livro{
+        if(this.compararIsbn(livro) === false){
+            this.livros.push(livro);
+        }
+        else{
+            throw new Error(`Livro com o ISBN '${livro.isbn}' já está cadastrado no sistema. Verifique e tente novamente'`)
+        }
+        return livro;
+    }
+
+    compararIsbn(livro: Livro): boolean{
+        for(let i: number = 0; i < this.livros.length; i++){
+            if(livro.isbn === this.livros[i].isbn){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    listar(titulo?: string, autor?: string, editora?: string, categoria_id?: string): Livro[]{
+        let livrosFiltrados: Livro [] = []
+
+        for(let i: number = 0; i < this.livros.length; i++){
+            livrosFiltrados.push(this.livros[i]);
+        }
+        
+        if(titulo){
+            let tituloFiltrado: Livro [] = [];
+            for(let i: number = 0; i < livrosFiltrados.length; i++){
+                if(livrosFiltrados[i].titulo.toLowerCase().includes(titulo.toLowerCase())){
+                    tituloFiltrado.push(livrosFiltrados[i]);
+                }
+            }
+            livrosFiltrados = tituloFiltrado;
+        }
+        if (autor) {
+            let autorFiltrado: Livro[] = [];
+            for (let i: number = 0; i < livrosFiltrados.length; i++) {
+                if (livrosFiltrados[i].autor.toLowerCase().includes(autor.toLowerCase())) {
+                    autorFiltrado.push(livrosFiltrados[i]);
+                }
+            }
+            livrosFiltrados = autorFiltrado;
+        }
+
+        if (editora) {
+            let editoraFiltrada: Livro[] = [];
+            for (let i: number = 0; i < livrosFiltrados.length; i++) {
+                if (livrosFiltrados[i].editora.toLowerCase().includes(editora.toLowerCase())) {
+                    editoraFiltrada.push(livrosFiltrados[i]);
+                }
+            }
+            livrosFiltrados = editoraFiltrada;
+        }
+
+        if (categoria_id) {
+            let categoriaFiltrada: Livro[] = [];
+            for (let i: number = 0; i < livrosFiltrados.length; i++) {
+                if (livrosFiltrados[i].categoria_id.nome.toLowerCase() === categoria_id.toLowerCase()) {
+                    categoriaFiltrada.push(livrosFiltrados[i]);
+                }
+            }
+            livrosFiltrados = categoriaFiltrada;
+        }
+        
+        return livrosFiltrados;
     }
 
     buscarIsbn(isbn: string): Livro{
         for(let i: number = 0; i < this.livros.length; i++){
-            const livro = this.livros[i];
-            if(livro.isbn === isbn){
-                return livro;
+            if(this.livros[i].isbn === isbn){
+                return this.livros[i];
             }
         }
         throw new Error(`Livro com o ISBN ${isbn} não encontrado. Verifique os dados e tente novamente`);
@@ -37,17 +96,45 @@ export class LivroRepository{
 
     buscarId(id: number): Livro {
         for (let i: number = 0; i < this.livros.length; i++) {
-            const livro = this.livros[i];
-            if (livro.id === id) {
-                return livro;
+            if(this.livros[i].id === id){
+                return this.livros[i];
             }
         }
         throw new Error(`Livro com o ID ${id} não encontrado. Verifique os dados e tente novamente.`);
     }   
 
-    atualizarIsbn(isbn: string, dados: Partial<Omit<Livro, 'id'>>): void{
-        const livro = this.buscarIsbn(isbn);
-        Object.assign(livro, dados);
+    atualizarIsbn(isbn: string, titulo?: string, autor?: string, editora?: string, categoria_id?: string): void{
+        let indexLivro: number = -1;
+        for (let i: number = 0; i < this.livros.length; i++) {
+            if (this.livros[i].isbn === isbn) {
+                indexLivro = i;
+            }
+        }
+
+        if (indexLivro === -1) {
+            throw new Error(`Livro com o ISBN '${isbn}' não encontrado. Verifique e tente novamente.`);
+        }
+
+        if (titulo === undefined && autor === undefined && editora === undefined && categoria_id === undefined) {
+            throw new Error("Nenhum parâmetro de atualização foi passado. Verifique e tente novamente.");
+        }
+
+        if (titulo) {
+            this.livros[indexLivro].titulo = titulo;
+        }
+
+        if (autor) {
+            this.livros[indexLivro].autor = autor;
+        }
+        
+        if (editora) {
+            this.livros[indexLivro].editora = editora;
+        }
+
+        if (categoria_id) {
+            let novaCategoria: CategoriaLivro = new CategoriaLivro(categoria_id);
+            this.livros[indexLivro].categoria_id = novaCategoria;
+        }
     }
 
     removerIsbn(isbn: string): void{
