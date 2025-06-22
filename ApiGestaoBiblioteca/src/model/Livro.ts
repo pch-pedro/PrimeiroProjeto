5d2a7400af31b63ef8b202e738aa910e89bb3d7d
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-22 18:04:28 -0300

    Ajustando Livro Model pra atribuir a categoria do livro através da string

--- ApiGestaoBiblioteca/src/model/Livro.ts
+++ ApiGestaoBiblioteca/src/model/Livro.ts
@@ -1,3 +1,5 @@
+import { CategoriaLivro } from "./CateoriaLivro";
+
 export class Livro{
     id: number;
     titulo: string;
@@ -5,7 +7,7 @@ export class Livro{
     editora: string;
     edicao: string;
     isbn: string;
-    categoria_id: number;
+    categoria_id: CategoriaLivro;
 
     constructor(titulo: string, autor: string, editora: string, edicao: string, isbn: string, categoria_id: string){
         this.id = this.criarId();
@@ -14,7 +16,7 @@ export class Livro{
         this.editora = editora;
         this.edicao = edicao;
         this.isbn = isbn;
-        this.categoria_id = categoria_id;
+        this.categoria_id = new CategoriaLivro(categoria_id);
     }
 
     criarId(){
