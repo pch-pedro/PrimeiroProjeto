d738663f29a4acf8251347d3c59d947f458e8446
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-22 17:58:01 -0300

    Restringindo categorias Livro

--- ApiGestaoBiblioteca/src/model/CateoriaLivro.ts
+++ ApiGestaoBiblioteca/src/model/CateoriaLivro.ts
@@ -1,9 +1,32 @@
 export class CategoriaLivro{
     id: number;
     nome: string;
-
-    constructor(id: number, nome: string){
-        this.id = id;
+    
+    constructor(nome: string){
+        nome = nome.toLowerCase();
+        this.verificarCategoria(nome);
+        this.id = this.definirId(nome);
         this.nome = nome;
     }
-}
+
+    definirId(nome: string): number{
+        if(nome === 'romance'){
+            return 1;
+        }
+        else if(nome === 'computação'){
+            return 2;
+        }
+        else if(nome === 'letras'){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
+    verificarCategoria(nome: string): void{
+        if(nome !== 'romance' && nome !== 'computação' && nome !== 'letras' && nome !== 'gestão'){
+            throw new Error(`Categoria de Usuário '${nome}' não é permitida. Certifique que a categoria do usuário seja uma dessas opções: 'romance' 'computação' 'letras' e 'gestão' todos com a acentuação correta`);
+        }
+    }
+};
--- ApiGestaoBiblioteca/src/model/Livro.ts
+++ ApiGestaoBiblioteca/src/model/Livro.ts
@@ -7,13 +7,17 @@ export class Livro{
     isbn: string;
     categoria_id: number;
 
-    constructor(id: number, titulo: string, autor: string, editora: string, edicao: string, isbn: string, categoria_id: number){
-        this.id = id;
+    constructor(titulo: string, autor: string, editora: string, edicao: string, isbn: string, categoria_id: string){
+        this.id = this.criarId();
         this.titulo = titulo;
         this.autor = autor;
         this.editora = editora;
         this.edicao = edicao;
         this.isbn = isbn;
         this.categoria_id = categoria_id;
+    }
+
+    criarId(){
+        return Date.now();
     }
 }
