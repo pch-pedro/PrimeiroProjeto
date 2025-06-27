2242cfe1e5f68bf5e5fe470c2c9753c903a8171f
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-06-26 21:47:00 -0300

    Criando pasta DataBase e instalando o MySql2

---
+++ ApiGestaoBiblioteca/src/database/mysql.ts
@@ -0,0 +1,19 @@
+import mysql, {Connection, QueryError } from 'mysql2';
+
+const dbConfig = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mysql',
+    database: 'vendas'
+};
+
+const mysqlConnection: Connection = mysql.createConnection(dbConfig);
+
+mysqlConnection.connect((err) => {
+    if(err){
+        console.error('Erro ao conectar ao banco de dados:', err);
+        throw err;
+    }
+    console.log('Conexao bem-sucedida com o banco de dados MySQL');
+});
